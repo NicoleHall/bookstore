4627b3f714c782089053f8b5810b9c5dbf0baa03
Nicole Hall <nicolealexandrahall@gmail.com>
2016-08-24 14:54:56 -0600

    filled in code for destroy, show, new and create. Added the corresponding new and show views

--- Nicole_s_bookstore/app/controllers/books_controller.rb
+++ Nicole_s_bookstore/app/controllers/books_controller.rb
@@ -5,12 +5,19 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    back = Location.find_by(storage_type: "back")
+    @book.location = back
+    @book.save
+    redirect_to books_path
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -19,8 +26,16 @@ class BooksController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
+    Book.delete(params[:id])
+    redirect_to books_path
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title)
+  end
+
 
 
 end
