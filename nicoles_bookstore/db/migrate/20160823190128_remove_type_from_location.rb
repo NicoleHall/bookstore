8cec2d12122b3ed5027c941ff17595fdceaedf9e
Nicole Hall <nicolealexandrahall@gmail.com>
2016-08-24 16:13:21 -0600

    removed the previous rails project where the routes were weird

--- nicoles_bookstore/db/migrate/20160823190128_remove_type_from_location.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromLocation < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :locations, :type, :string
-  end
-end
--- nicoles_bookstore/db/migrate/20160823190245_add_location_type_to_location.rb
+++
@@ -1,5 +0,0 @@
-class AddLocationTypeToLocation < ActiveRecord::Migration[5.0]
-  def change
-    add_column :locations, :storage_type, :string
-  end
-end
