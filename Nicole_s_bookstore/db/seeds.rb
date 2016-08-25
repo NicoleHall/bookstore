092097c7efdffccf6f1be6dfa7c2e9f4ddf806e5
Nicole Hall <nicolealexandrahall@gmail.com>
2016-08-25 12:59:19 -0600

    added seed data for 6 shelves and 4 tables

--- Nicole_s_bookstore/db/seeds.rb
+++ Nicole_s_bookstore/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ExactPlacement.destroy_all
 Location.destroy_all
 
 Location.create!([{
@@ -18,3 +19,31 @@ Location.create!([{
 }])
 
 p "Created #{Location.count} locations"
+
+shelf = Location.find_by(storage_type: "shelf").id
+table = Location.find_by(storage_type: "table").id
+
+6.times do |num|
+  ExactPlacement.create!(name: "shelf #{num + 1}", location_id: shelf)
+end
+
+4.times do |num|
+  ExactPlacement.create!(name: "table #{num + 1}", location_id: table)
+end
+
+ExactPlacement.all.each do |ep|
+  p ep.name
+end
+
+# expected console output after running seed file
+# "Created 3 locations"
+# "shelf 1"
+# "shelf 2"
+# "shelf 3"
+# "shelf 4"
+# "shelf 5"
+# "shelf 6"
+# "table 1"
+# "table 2"
+# "table 3"
+# "table 4"
